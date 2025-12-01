b51709c7b36cfc9bc2e93f868c268c277f0f9fab
husamettinarabaci <harabaci88@gmail.com>
2025-12-01 22:32:48 +0300

    feat(core): implement button group component
    sdf sadfd SDSDF sdfsdf SFD DSFDS  SFD SFDf

--- hexawebshare/src/components/core/buttons/ButtonGroup.svelte
+++ hexawebshare/src/components/core/buttons/ButtonGroup.svelte
@@ -2,3 +2,30 @@
 SPDX-FileCopyrightText: 2025 hexaTune LLC
 SPDX-License-Identifier: MIT
 -->
+
+
+|
+afterUpdate(() => {
+  
+  afterUpdate(() => {
+    afterUpdate(() => {
+      afterUpdate(() => {
+        const fsadfsd
+        fsadfsdsdfadsf
+          |fsadfsdsdfadsf
+
+            fsadfsdsdfadsf
+
+          werewe
+
+
+          fsadfsdsdfadsf
+
+          = readable(initialValue, () => {
+          console.log('got a subscriber');
+          return () => console.log('no more subscribers');
+        });
+      });
+    });
+  });
+});
