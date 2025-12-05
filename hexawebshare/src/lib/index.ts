f4403773556aaae37afb185f360e11a7575bae7d
ayşeturmusn <trmsays@gmail.com>
2025-12-05 23:33:36 +0300

    fix(core): export ButtonGroup component in index.ts

--- hexawebshare/src/lib/index.ts
+++ hexawebshare/src/lib/index.ts
@@ -2,4 +2,5 @@
 // SPDX-License-Identifier: MIT
 
 // Reexport your entry components here
+export { default as ButtonGroup } from '../components/core/buttons/ButtonGroup.svelte';
 export { default as ProgressBar } from '../components/core/feedback/ProgressBar.svelte';
