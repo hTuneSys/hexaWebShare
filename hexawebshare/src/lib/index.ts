4f5b06076460d0fe15f914101905d3efd6809c2b
krmlp <keremalparslan@gmail.com>
2025-12-09 18:50:34 +0300

    feat(core): export Icon component in src/lib/index.ts

--- hexawebshare/src/lib/index.ts
+++ hexawebshare/src/lib/index.ts
@@ -5,3 +5,6 @@
 
 // Core - Feedback
 export { default as Spinner } from '../components/core/feedback/Spinner.svelte';
+
+// Core - Media
+export { default as Icon } from '../components/core/media/Icon.svelte';
