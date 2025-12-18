b8ba52fbe04d2f0ff1ac3eafa1fdd7df1d9adf5f
krmlp <keremalparslan@gmail.com>
2025-12-18 19:34:23 +0300

    feat(core): implement Breadcrumbs component with Storybook stories

--- hexawebshare/src/lib/index.ts
+++ hexawebshare/src/lib/index.ts
@@ -2,3 +2,7 @@
 // SPDX-License-Identifier: MIT
 
 // Reexport your entry components here
+
+// Core - Overlay Navigation
+export { default as Breadcrumbs } from '../components/core/overlay-navigation/Breadcrumbs.svelte';
+export type { BreadcrumbItem } from '../components/core/overlay-navigation/Breadcrumbs.svelte';
