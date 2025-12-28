468be21062ca7e386e963107686294ad1e034474
Hatice Babaoğlu <100375578+haticebabaoglu@users.noreply.github.com>
2025-12-28 12:57:36 +0300

    feat(core): implement Steps component with Storybook stories (#332)

--- hexawebshare/src/lib/index.ts
+++ hexawebshare/src/lib/index.ts
@@ -1,2 +1,6 @@
 // SPDX-FileCopyrightText: 2025 hexaTune LLC
 // SPDX-License-Identifier: MIT
+
+// Core / Overlay Navigation
+export { default as Steps } from '../components/core/overlay-navigation/Steps.svelte';
+export type { StepItem } from '../components/core/overlay-navigation/Steps.svelte';
